9776c16c38d2f3df06e291cb32477b0234160f6e
Tim Pavlov <tema.pavlov.2000@mail.ru>
2019-07-05 03:37:25 +0300

    managed many_to_many relations in models, created migration_service which is used for old database schema password encryption and storing it in the new database schema, modified bank module, finished loan module, created messaging module,

--- bin/app/core/decorators/model.ts
+++
@@ -1,9 +0,0 @@
-import DBModel from "../dbmodel";
-
-export default function model(
-    dbModel: new <T extends DBModel>(model: { [key: string]: any }) => T
-) {
-    return (target, value: string) => {
-
-    };
-}
